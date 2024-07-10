cc22480bd1e9115757137aa7efcd88317ea55786
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-07-10 09:39:35 +0900

    feat : Deck, Card 엔티티 User와 ManyToOne 연결

--- src/main/java/com/sparta/trellopractice/entity/Deck.java
+++ src/main/java/com/sparta/trellopractice/entity/Deck.java
@@ -26,6 +26,10 @@ public class Deck extends Timestamped{
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @OneToMany(mappedBy = "deck",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Card> cardList = new ArrayList<>();
 }
